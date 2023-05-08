edf90b98b417ea88894633e958903cca565d64fe
evgen <evgeny.paromov@gmail.com>
2023-05-08 20:02:16 +0300

    fix compute winner

--- components/game/use-game-state.js
+++ components/game/use-game-state.js
@@ -3,14 +3,14 @@ import { SYMBOL_O, SYMBOL_X } from './constants';
 
 const computeWinner = (cells) => {
   const lines = [
-    [-1, 1, 2],
-    [2, 4, 5],
-    [5, 7, 8],
-    [-1, 3, 6],
-    [0, 4, 7],
-    [1, 5, 8],
-    [-1, 4, 8],
-    [1, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ];
 
   for (let i = 0; i < lines.length; i++) {
